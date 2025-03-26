1060fd95bbf9ca0e5401f133f1f0cd47cc575114
Your Name <nadinekeyrouz0011@gmail.com>
2025-03-26 11:28:27 +0200

    p

--- app/components/imageswtext.js
+++ app/components/imageswtext.js
@@ -3,7 +3,7 @@
     return (
         <div className="container mx-auto relative h-full  flex items-center justify-center bg-black text-white py-12">
       
-      <div className="grid grid-cols-1 sm:grid-cols-2  w-full gap-10 sm:gap-32 ">
+      <div className="grid grid-cols-1 sm:grid-cols-2  p-4 sm:p-0 w-full gap-10 sm:gap-28 ">
         {items.map((item, index) => (
           <div key={index} className="overflow-hidden">
             <img src={item.image} alt={item.text} className="w-full h-60 object-cover" />
