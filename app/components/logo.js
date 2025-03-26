fd7399a765259d33299025611584a2144fe71cd1
Your Name <nadinekeyrouz0011@gmail.com>
2025-03-26 14:05:24 +0200

    p

--- app/components/logo.js
+++ app/components/logo.js
@@ -1,7 +1,7 @@
 export default function Logo() {
     return (
-      <svg width="70" height="70" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="50" cy="50" r="40" stroke="white" strokeWidth="2" fill="none"/>
+      <svg width="80" height="70" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="50" cy="50" r="50" stroke="white" strokeWidth="2" fill="none"/>
         <text x="50" y="50" fontSize="" fontFamily="Arial" fill="white" textAnchor="middle" fontWeight="bold" dy=".3em">
           mind space
         </text>
