5ed13f7b88d504e397c64e57eafa218138054fc7
Your Name <nadinekeyrouz0011@gmail.com>
2025-04-03 11:31:22 +0300

    p

--- app/components/swipper.js
+++ app/components/swipper.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Autoplay } from "swiper/modules";
+import { Pagination, Autoplay,Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
@@ -19,31 +19,31 @@ const CardsSwipperComponent = ({ slides }) => {
   return (
     <div className="container mx-auto p-4 sm:p-0  md:mt-37  md:mb-37">
       <Swiper
-        modules={[Pagination, Autoplay]}
+        modules={[Pagination, Autoplay,Navigation]}
         pagination={{ clickable: false }}
+        navigation={{ clickable: true }}
         autoplay={{ delay: 6000 }}
         breakpoints={{
             0: { slidesPerView: 1 },
             768: { slidesPerView: 2 },
-            1024: { slidesPerView: 3 },
+            1024: { slidesPerView: 3  },
           }}
         loop
-        className="w-full pb-20 sm:h-[500px] mt-auto"
+        className="w-full pb-20 sm:h-[600px] "
       >
         {slides.data.map((slide, index) => (
           <SwiperSlide 
             key={slide.id || index} 
-          
-     
+   className=" p-9"
           >
             <img 
               src={slide.image || "/default-image.jpg"} 
               alt={slide.title || "Project Image"}
-              className="w-full h-96 sm:h-[500px] object-fit sm:w-[90%] transition-transform duration-300 hover:scale-105"
+              className="w-full h-96 sm:h-[500px] object-fit sm:w-[100%] transition-transform duration-300 hover:scale-105"
             />
         <div className="absolute inset-0 flex items-center justify-center bg-opacity-50 ">
-  <div className="text-center">
-    <h2 className="text-white text-3xl w-[90%] font-bold mb-4">{slide.title}</h2>
+  <div className="text-center justify-items-center">
+    <h2 className="text-white text-3xl w-[70%]  font-bold mb-4">{slide.title}</h2>
     <button
       onClick={() => router.push(`/aboutUs/${slide.id}`)}
       className="px-6 cursor-pointer py-2 border-[2px] border-[#FF4A4A] text-white text-lg font-semibold  hover:bg-[#e03e3e] transition duration-300 ease-in-out"
--- app/pages/about/[id].js
+++ app/pages/about/[id].js
@@ -58,14 +58,24 @@ export default function ProjectDetail({ params }) {
       <Header activeSection="aboutus" links={links} />
 
       <div className="container mx-auto p-4 sm:p-0">
-        <div className="flex flex-col sm:flex-row space-x-7 space-y-2">
-          <div className="flex flex-col sm:w-[50%] items-center justify-center w-full bg-white sm:p-6">
-            <h1 className="text-4xl font-bold text-center">{project.title}</h1>
-            <p className="mt-4 text-center">{project.description}</p>
-          </div>
-          <div className="sm:w-[50%]">
+        <div className="flex flex-col  space-x-7 space-y-8 text-white">
+        <div className="sm:w-[100%]">
             <img src={project.image} alt={project.title} className="w-full h-96 object-cover" />
           </div>
+          <h1 className="text-xl font-semi-bold text-center ">{project.title}</h1>
+          <div className="flex flex-col sm:flex-row justify-between items-stretch w-full space-x-36">
+
+  <div className="flex flex-col sm:text-2xl font-bold w-full  flex-grow">
+    <h1 className="mt-4">{project.seo_description}</h1>
+  </div>
+
+  <div className="flex flex-col sm:text-2xl font-bold w-full  flex-grow justify-end justify-self-end sm:text-right">
+    <h1 className="mt-4">CLIENT: {project.client}</h1>
+    <h1 className="mt-4">DIRECTOR(S): {project.directors}</h1>
+    <h1 className="mt-4">AGENCY: {project.agency}</h1>
+  </div>
+</div>
+
         </div>
       </div>
 
